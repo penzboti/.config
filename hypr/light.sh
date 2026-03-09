473f83ecd46270c032b07be5fa0cb71cecbe2a1c
penzboti <penzboti@gmail.com>
2026-03-09 17:13:14 +0100

    HYPR: changed from light to brighnessctl

---
+++ hypr/light.sh
@@ -0,0 +1,11 @@
+# min is 960 (i dont know who came up with this number, but i hate him)
+# we go by percentages, but cant get current percentage
+# minimum and maximum is handled by brightnessctl
+
+current=$(brightnessctl get)
+
+if [ "$1" = "-" ]; then
+  brightnessctl --min-value=960 -- set 5%-
+elif [ "$1" = "+" ]; then
+  [ $current = "960" ] && brightnessctl set 4%+ || brightnessctl set 5%+
+fi
--- hypr/light_up.sh
+++
@@ -1,2 +0,0 @@
-[ $(light -G | cut -c1-2) = "1." ] && light -A 4 || light -A 5
-# way too much work for something not even important
